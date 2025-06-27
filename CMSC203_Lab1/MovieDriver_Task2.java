898567faf7012da196742edaf7a2416d058d0268
Oğuz Can Yılmazlar <yilmazlaroguzcan@gmail.com>
2025-06-27 05:38:20 -0400

    Add files via upload

--- CMSC203_Lab1/MovieDriver_Task2.java
+++ CMSC203_Lab1/MovieDriver_Task2.java
@@ -1,3 +1,15 @@
+/*
+ * Class: CMSC203 40539
+ * Instructor: Grigoriy Grinberg
+ * Description: (Give a brief description for each Class) Ask for movie title, rating, and sold ticket amount and print them.
+ * Due: 06/30/2025
+ * Platform/compiler: MacOS openjdk temurin 24.0.1
+ * I pledge that I have completed the programming assignment
+ * independently. I have not copied the code from a student or   * any source. I have not given my code to any student.
+ * Print your Name here: Oguz Can Yilmazlar
+ */
+
+
 import java.util.Scanner;
 
 public class MovieDriver_Task2 {
